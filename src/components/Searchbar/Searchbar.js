0887c767e9398a75a727c5ca6d7e08e2c3e917cb
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-21 23:25:45 +0300

    add icon

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -5,12 +5,14 @@ import {
   SearchFormBtnLabel,
   SearchFormInput,
 } from './Searchbar.styled';
+import { BiSearch } from 'react-icons/bi';
 
 export const Searchbar = ({ onSubmit, inputValue, onChange }) => {
   return (
     <Layout>
       <SearchForm onSubmit={onSubmit}>
         <SearchFormBtn type="submit">
+          <BiSearch size="22" />
           <SearchFormBtnLabel>Search</SearchFormBtnLabel>
         </SearchFormBtn>
 
