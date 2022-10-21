5f439f4780b2d118aac4e1686b6ea606cd52934e
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-22 01:04:15 +0300

    add component ImageGalleryItem

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,9 @@
+import { ImageBox, GalleryItemImage } from './ImageGalleryItem.styled';
+
+export const ImageGalleryItem = () => {
+  return (
+    <ImageBox>
+      <GalleryItemImage src="" alt="" />
+    </ImageBox>
+  );
+};
